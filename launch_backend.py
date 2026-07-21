fa9a3a68ab6e4effd815ae490e434655076ce0f7
cabincrewmorocco-beep <rachidelsabah@gmail.com>
2026-07-21 21:44:07 +0100

    comit

---
+++ launch_backend.py
@@ -0,0 +1,6 @@
+"""Launcher that strips Hermes venv from sys.path to avoid pydantic_core conflict."""
+import sys
+sys.path = [p for p in sys.path if "hermes-agent" not in p]
+
+from agentic_os.__main__ import main
+main()
